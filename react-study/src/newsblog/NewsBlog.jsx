7b2cd8aa3f9131396f65267670479e0fdf3e443e
Yong-jin <takeardor@gmail.com>
2026-06-16 14:30:41 +0900

    포스트 삭제 기능 추가(splice, filter)

--- react-study/src/newsblog/NewsBlog.jsx
+++ react-study/src/newsblog/NewsBlog.jsx
@@ -63,6 +63,37 @@ function NewsBlog() {
 
                             }}>❤</span> {likeCountArr[index]}  </h4>
                             <p>내용자리</p>
+                            <button onClick={()=>{
+                                //누른 대상을 삭제
+                                //배열안에 누른 대상의 index 위치에 있는 값을 제거
+
+                                // index 
+
+                                // arr.splice(index, 몇개지울, 추가할값)
+                                //             0,    1
+                                //             5,    0,        10
+                                //             3,    1,        3
+
+                                
+                                //뉴스제목
+                                // let temp = [...news];
+                                // temp.splice(index, 1);  //삭제를 누른 index위치에 값 1개 삭제
+                                // setNews(temp);
+
+                                // //좋아요 갯수 같이 삭제
+                                // likeCountArr.splice(index, 1);
+                                                    // 2 index 
+                                let temp = [...news];
+                                temp = temp.filter((value, idx)=>{
+                                    return idx != index;   // 0 != 2   1 != 2  2 != 2
+                                })
+                                console.log(temp);
+                                setNews(temp);
+
+                                setLikeCountArr( likeCountArr.filter(( _ , idx) => idx != index ))
+                                
+
+                            }}>삭제</button>
                         </div>
                     )
                 })
